a565d4b1fe63d508b85b6d02a312bf841ba27ab2
Constantin-Cristian DUMITRIU <cdumitriu@pentalog.com>
2019-12-12 18:29:35 +0200

    add book page

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Book;
+use Symfony\Component\HttpFoundation\Response;
 
 class BooksController extends AbstractController
 {
@@ -15,13 +16,33 @@ class BooksController extends AbstractController
     {
         $repository = $this->getDoctrine()->getRepository(Book::class);
         $books = $repository->findAll();
-       // dump($books); die();
 
         return $this->render('books/index.html.twig', [
             'controller_name' => 'BooksController',
             'books' => $books
         ]);
     }
+
+    /**
+     * @Route("/book/{id}", name="book")
+     */
+    public function book($id){
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        return $this->render('books/details.html.twig', [
+               'book' => $book
+        ]);
+
+    }
+
+
+
 
 
 }
